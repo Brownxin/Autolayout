e8856d7a5739a083fd86b99892eae8d07cbe7929
Xin Wang <kevin.wx86@gmail.com>
2016-02-24 11:22:04 -0500

    Swift Learning---Autolayout

--- Autolayout/ViewController.swift
+++ Autolayout/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var secure: Bool = false {
+        didSet{
+            updateUI()
+        }
     }
-
-
+    
+    @IBAction func toggle(sender: UIButton) {
+        secure = !secure
+    }
+    
+    private func updateUI() {
+        passwordField.secureTextEntry = true
+        passwordLabel.text = secure ? "Secured Password" : " Password"
+    }
 }
 
